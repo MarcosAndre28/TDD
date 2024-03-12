7faecac110fb97b4971f3af7a751616d6fb53a8e
MarcosAndre28 <marcosandre1728@gmail.com>
2024-03-12 07:40:52 -0300

    Test: Cubit create users and getUsers

--- test/src/authentication/presentation/cubit/authentication_cubit_teste_test.dart
+++ test/src/authentication/presentation/cubit/authentication_cubit_teste_test.dart
@@ -1,5 +1,8 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:tdd_tutorial/core/errors/failure.dart';
 import 'package:tdd_tutorial/src/authentication/domain/usecases/create_user.dart';
 import 'package:tdd_tutorial/src/authentication/domain/usecases/get_users.dart';
 import 'package:tdd_tutorial/src/authentication/presentation/cubit/authentication_cubit.dart';
@@ -13,6 +16,9 @@ void main() {
   late MockGetUsers mockGetUsers;
   late AuthenticationCubit cubit;
 
+  const tCreateUserParams = CreateUseParams.empty();
+  const tAPIFailure = APIFailure(message: 'API Failure', statusCode: 400);
+
   setUp(
     () {
       mockCreateUser = MockCreateUser();
@@ -21,6 +27,7 @@ void main() {
         createUser: mockCreateUser,
         getUsers: mockGetUsers,
       );
+      registerFallbackValue(tCreateUserParams);
     },
   );
 
@@ -30,6 +37,93 @@ void main() {
     'initial state should be [AuthenticationInitial]',
     () async {
       expect(cubit.state, const AuthenticationInitial());
+    },
+  );
+
+  group(
+    'createUser',
+    () {
+      blocTest<AuthenticationCubit, AuthenticationState>(
+        'should emit [CreatingUser, UserCreated] when successful',
+        build: () {
+          when(() => mockCreateUser(any()))
+              .thenAnswer((_) async => const Right(null));
+          return cubit;
+        },
+        act: (cubit) => cubit.createUser(
+          createdAt: tCreateUserParams.createdAt,
+          name: tCreateUserParams.name,
+          avatar: tCreateUserParams.avatar,
+        ),
+        expect: () => const [
+          CreateUserLoadingState(),
+          CreateUserSuccessState(),
+        ],
+        verify: (_) {
+          verify(() => mockCreateUser(tCreateUserParams)).called(1);
+          verifyNoMoreInteractions(mockCreateUser);
+        },
+      );
+
+      blocTest<AuthenticationCubit, AuthenticationState>(
+        'should emit [CreatingUser, AuthenticationError] when unsuccesssfull',
+        build: () {
+          when(() => mockCreateUser(any()))
+              .thenAnswer((_) async => const Left(tAPIFailure));
+          return cubit;
+        },
+        act: (cubit) => cubit.createUser(
+          createdAt: tCreateUserParams.createdAt,
+          name: tCreateUserParams.name,
+          avatar: tCreateUserParams.avatar,
+        ),
+        expect: () => [
+          const CreateUserLoadingState(),
+          AuthenticationErrorState(tAPIFailure.errorMessage),
+        ],
+        verify: (_) {
+          verify(() => mockCreateUser(tCreateUserParams)).called(1);
+          verifyNoMoreInteractions(mockCreateUser);
+        },
+      );
+    },
+  );
+  group(
+    'getUsers',
+    () {
+      blocTest<AuthenticationCubit, AuthenticationState>(
+        'should emit [GettingUsers, UsersLoaded] when successful',
+        build: () {
+          when(() => mockGetUsers()).thenAnswer((_) async => const Right([]));
+          return cubit;
+        },
+        act: (cubit) => cubit.getUsers(),
+        expect: () => const [
+          GettingUsersLoadingState(),
+          UsersLoadedState([]),
+        ],
+        verify: (_) {
+          verify(() => mockGetUsers()).called(1);
+          verifyNoMoreInteractions(mockGetUsers);
+        },
+      );
+      blocTest<AuthenticationCubit, AuthenticationState>(
+        'should emit [GettingUsers, AuthenticationError] when unsuccesssfull',
+        build: () {
+          when(() => mockGetUsers())
+              .thenAnswer((_) async => const Left(tAPIFailure));
+          return cubit;
+        },
+        act: (cubit) => cubit.getUsers(),
+        expect: () => [
+          const GettingUsersLoadingState(),
+          AuthenticationErrorState(tAPIFailure.errorMessage),
+        ],
+        verify: (_) {
+          verify(() => mockGetUsers()).called(1);
+          verifyNoMoreInteractions(mockGetUsers);
+        },
+      );
     },
   );
 }
